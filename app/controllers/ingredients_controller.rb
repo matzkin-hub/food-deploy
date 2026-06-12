8e38194dc02da67745f41261ab6a53335004b21b
matzkin <matsutani.kazuki@sonicgarden.jp>
2026-06-12 16:37:56 +0900

    食材一覧ページを追加し、IngredientsControllerを更新してユーザーの食材を表示する機能を実装

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,2 +1,6 @@
 class IngredientsController < ApplicationController
+  before_action :authenticate_user!
+  def index
+    @ingredients = current_user.ingredients.includes(:ingredient_stocks)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'up' => 'rails/health#show', as: :rails_health_check
+  resources :ingredients do
+    resources :ingredient_stocks
+  end
 end
