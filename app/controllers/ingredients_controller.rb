41e28844535ad102af60de75676f1a8745965bd6
matzkin <matsutani.kazuki@sonicgarden.jp>
2026-06-13 15:56:09 +0900

    食材管理機能を実装し、CRUD操作を追加。新規作成、編集、削除機能を含む。

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -3,4 +3,45 @@ class IngredientsController < ApplicationController
   def index
     @ingredients = current_user.ingredients.includes(:ingredient_stocks)
   end
+
+  def new
+    @ingredient = Ingredient.new
+  end
+
+  def create
+    @ingredient = current_user.ingredients.build(ingredient_params)
+    if @ingredient.save
+      redirect_to ingredients_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @ingredient = Ingredient.find(params[:id])
+  end
+
+  def update
+    @ingredient = Ingredient.find(params[:id])
+    if @ingredient.update(ingredient_params)
+      redirect_to ingredients_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @ingredient = Ingredient.find(params[:id])
+    if @ingredient.destroy!
+      redirect_to ingredient_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def ingredient_params
+    params.require(:ingredient).permit(:name)
+  end
 end
