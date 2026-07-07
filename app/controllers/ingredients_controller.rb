f12fcf06a01f09bb8eae6d1c705f5bf4196bc973
matzkin <matsutani.kazuki@sonicgarden.jp>
2026-07-07 18:18:31 +0900

    食材一覧のページネーションを12から8に変更し、Kaminariのページネーション表示を改善しました。

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -4,7 +4,7 @@ class IngredientsController < ApplicationController
   before_action :authenticate_user! # ログインしているユーザしかできない
   before_action :set_id, only: %i[edit update destroy] # 特定のユーザしかできないアクション
   def index
-    @ingredients = current_user.ingredients.left_joins(:ingredient_stocks).includes(:ingredient_stocks).group('ingredients.id').order('MIN(ingredient_stocks.expire_on) ASC NULLS LAST').page(params[:page]).per(12)
+    @ingredients = current_user.ingredients.left_joins(:ingredient_stocks).includes(:ingredient_stocks).group('ingredients.id').order('MIN(ingredient_stocks.expire_on) ASC NULLS LAST').page(params[:page]).per(8)
   end
 
   def new
